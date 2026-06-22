2f9a3df72d12246628acbb47e01ad5b618c551b0
Ashish <ashish@gmail.com>
2026-06-22 12:31:30 +0530

    files modified....

--- app.py
+++ app.py
@@ -1,7 +1,48 @@
+# from flask import Flask, render_template, request
+# from gtts import gTTS
+# from io import BytesIO
+# from langdetect import detect
+# import base64
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @app.route("/convert", methods=["POST"])
+# def convert():
+#     text = request.form["text"]
+
+#     try:
+#         lang = detect(text)
+#     except:
+#         lang = "en"
+
+#     if lang == "hi":
+#         tts = gTTS(text=text, lang="hi")
+#     else:
+#         tts = gTTS(text=text, lang="en", tld="co.in")
+
+#     mp3_fp = BytesIO()
+#     tts.write_to_fp(mp3_fp)
+
+#     audio_data = base64.b64encode(
+#         mp3_fp.getvalue()
+#     ).decode()
+
+#     return render_template(
+#         "index.html",
+#         audio_data=audio_data
+#     )
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 from flask import Flask, render_template, request
-from gtts import gTTS
-from io import BytesIO
 from langdetect import detect
+import edge_tts
+import asyncio
 import base64
 
 app = Flask(__name__)
@@ -10,31 +51,63 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+
 @app.route("/convert", methods=["POST"])
 def convert():
+
     text = request.form["text"]
+    gender = request.form["gender"]
+    speed = request.form["speed"]
 
     try:
         lang = detect(text)
     except:
         lang = "en"
 
+    # Hindi Voices
     if lang == "hi":
-        tts = gTTS(text=text, lang="hi")
-    else:
-        tts = gTTS(text=text, lang="en", tld="co.in")
+        if gender == "male":
+            voice = "hi-IN-MadhurNeural"
+        else:
+            voice = "hi-IN-SwaraNeural"
 
-    mp3_fp = BytesIO()
-    tts.write_to_fp(mp3_fp)
+    # English Voices
+    else:
+        if gender == "male":
+            voice = "en-IN-PrabhatNeural"
+        else:
+            voice = "en-IN-NeerjaNeural"
+
+    audio_bytes = asyncio.run(
+        generate_audio(text, voice, speed)
+    )
 
     audio_data = base64.b64encode(
-        mp3_fp.getvalue()
-    ).decode()
+        audio_bytes
+    ).decode("utf-8")
 
     return render_template(
         "index.html",
         audio_data=audio_data
     )
+
+
+async def generate_audio(text, voice, speed):
+
+    communicate = edge_tts.Communicate(
+        text=text,
+        voice=voice,
+        rate=speed
+    )
+
+    audio_data = b""
+
+    async for chunk in communicate.stream():
+        if chunk["type"] == "audio":
+            audio_data += chunk["data"]
+
+    return audio_data
+
 
 if __name__ == "__main__":
     app.run(debug=True)
